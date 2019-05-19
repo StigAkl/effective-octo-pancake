e172563e4015880db18d47a08ed812cee0cdbc56
Stig Andreas Langeland <8777406+StigAkl@users.noreply.github.com>
2019-05-19 18:01:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'reeact';
+import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import LandingPage from './Components/landingpage';
 import AppLayout from './Components/applayout';  
