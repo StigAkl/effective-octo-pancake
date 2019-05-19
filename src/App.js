470659c81823edba8ae04780c50524f247b722ce
Stigiboy <stg@hotmail.no>
2019-05-19 15:34:05 +0200

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import LandingPage from './Components/landingpage'; 
 
 class App extends Component {
 
   render() {
   return (
     <div className="App">
+      <h1>Protected React Router</h1>
+      <Route component={LandingPage} />
     </div>
   );
   }
